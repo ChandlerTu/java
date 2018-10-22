16142a20ea2f0f3e061769c5bb8efdb3d937512e
Chandler Tu <chandlertu@chandlertu.com>
2018-10-22 13:40:39 +0800

    Commit

---
+++ src/test/java/com/chandlertu/java/io/StackTest.java
@@ -0,0 +1,16 @@
+package com.chandlertu.java.io;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
+public class StackTest {
+	
+	@Test
+	public void test()
+	{
+		Stack<String> stack = new Stack<String>();
+		
+	}
+
+}
